d08e38932e22c914612a03ac67f3db24efcea6b1
yangmiemie2017 <2415119609@qq.com>
2017-11-21 17:46:25 +0800

    Initial commit

---
+++ src/main/java/com/spring_react_demo/SpringReactDemoApplication.java
@@ -0,0 +1,12 @@
+package com.spring_react_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactDemoApplication.class, args);
+	}
+}
